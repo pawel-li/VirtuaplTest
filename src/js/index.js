80457777838aa00ed44c3b1dad8f8f455afa1c06
unknown <>
2021-06-11 10:37:09 +0200

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 /* Here goes your JS code */
+const popupDiv = document.querySelector('.popup');
+const showPopupBtn = document.getElementById('show-popup-form');
+const closePopupBtn = popupDiv.querySelector('.popup__close');
+
+//shop popup on click
+showPopupBtn.addEventListener('click', function(){
+  popupDiv.classList.add('popup--active');
+});
+
+//close popup when clicks 'X' button
+closePopupBtn.addEventListener('click', function(){
+  popupDiv.classList.remove('popup--active');
+});
